aafb5f96f084c33b65e4ea62627e10041769629a
EC2 Default User <ec2-user@ip-172-31-45-77.sa-east-1.compute.internal>
2018-09-14 18:40:20 +0000

    adicionando chamada ao servico stravaWrap

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ const chatbot = new Telegram.Telegram('626714518:AAFVXrEKwjgg1A9T_Vw-1JU_bMa1D9A
 
 class EventsController extends TelegramBaseController{
 	
+	callStravaWrap(param, scope){
+	    var url = "https://stravawrap.herokuapp.com/messages/5b9badc1e90db000161ab975"; 
+
+	    const axios = require('axios');
+
+	    axios.get(url)
+	         .then(response => {
+		   let msg = 'voce solicitou a mensagem '+param+', que tem o valor: '+ (response.data.title)
+		   scope.sendMessage(msg)
+		 })
+		 .catch(error => {
+		   console.log(error);
+		 });
+
+	}
+
+
 	olaAction(scope) {
 	    const param = scope._message._text.substring(5)
 	    let msg = `bababba -- carara ${param}`
@@ -15,11 +32,32 @@ class EventsController extends TelegramBaseController{
 	    scope.sendMessage(msg)
 	}
 
+	msgAction(scope){
+	    const param = scope._message._text.substring(5)
+	    let msg = this.callStravaWrap(param, scope)
+	    console.log(msg)
+	    scope.sendMessage(msg)
+
+	}
 	get routes() {
 		return {
-		'ola': 'olaAction'
+		'ola': 'olaAction',
+		'oi' : 'olaAction',
+		'msg': 'msgAction'
 		}
 	}
 }
 
+class OtherwiseController extends TelegramBaseController {
+    handle(scope){
+        console.log('otherwise')
+	const msg = scope._message._text
+	console.log(msg)
+	scope.sendMessage(`enfia seu ${msg} no teu cu! FDP!`)
+    }
+}
+
 chatbot.router.when( new TextCommand('/ola','ola'), new EventsController() )
+		.when( new TextCommand('oi','oi'), new EventsController())
+		.when( new TextCommand('/msg','msg'), new EventsController())
+		.otherwise(new OtherwiseController())
